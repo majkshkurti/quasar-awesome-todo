8f5d849c24b417921f3d760357175e784c5e6b69
Majk <majkshkurti94@gmail.com>
2019-10-25 15:48:34 +0200

    implemented navigation for web

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,70 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          icon="menu"
+          aria-label="Menu"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+    <q-footer>
+      <q-tabs>
+        <q-route-tab icon="list" label="Todo" to="/" exact />
+        <q-route-tab icon="settings" label="Settings" to="/settings" exact />
+      </q-tabs>
+    </q-footer>
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+        <q-item-label header>Navigation</q-item-label>
+        <q-item clickable to="/" exact>
+          <q-item-section avatar>
+            <q-icon name="list" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Todo</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable to="/settings" exact>
+          <q-item-section avatar>
+            <q-icon name="settings" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Settings</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "Layout",
+
+  data() {
+    return {
+      leftDrawerOpen: false
+    };
+  }
+};
+</script>
